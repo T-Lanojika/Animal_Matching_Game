6dc10c475f39c2c4f8ef6786625bc6b62d43cdf1
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2024-11-24 12:33:16 +0000

    some error fixed

--- animal_matching_game/src/App.js
+++ animal_matching_game/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Result from './components/Result';
 
-
 function App() {
   return (
     <div className="App">
@@ -9,6 +8,7 @@ function App() {
       <Result />
 
     </div>
+    
   );
 }
 
