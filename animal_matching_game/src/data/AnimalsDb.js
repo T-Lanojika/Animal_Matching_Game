f8724a494b5e419cae63e1214895730de89f07fc
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2024-11-24 12:40:27 +0000

    some errors fixed

--- animal_matching_game/src/data/AnimalsDb.js
+++ animal_matching_game/src/data/AnimalsDb.js
@@ -1,18 +1,19 @@
-export const animals=[
-    {name:'dog',img:'../assets/img/1.jpg'},
-    {name:'rabbit',img:'2.jpg'},
-    {name:'tiger',img:'3.jpg'},
-    {name:'giraffe',img:'4.jpg'},
-    {name:'lion',img:'5.jpg'},
-    {name:'panda',img:'6.jpg'},
-    {name:'owl',img:'7.jpg'},
-    {name:'elephant',img:'8.jpg'},
-    {name:'cat',img:'9.jpg'},
-    {name:'sheep',img:'10.jpg'},
-    {name:'donkey',img:'11.jpg'},
-    {name:'fox',img:'12.jpg'},
-    {name:'wolf',img:'13.jpg'},
-    {name:'bear',img:'14.jpg'},
-    {name:'hen',img:'15.jpg'},
-    {name:'deer',img:'16.jpg'}
-];
+export const animals = [
+    { name: "dog", img: require("../assets/img/1.jpg") },
+    { name: "rabbit", img: require("../assets/img/2.jpg") },
+    { name: "tiger", img: require("../assets/img/3.jpg") },
+    { name: "giraffe", img: require("../assets/img/4.jpg") },
+    { name: "lion", img: require("../assets/img/5.jpg") },
+    { name: "panda", img: require("../assets/img/6.jpg") },
+    { name: "owl", img: require("../assets/img/7.jpg") },
+    { name: "elephant", img: require("../assets/img/8.jpg") },
+    { name: "cat", img: require("../assets/img/9.jpg") },
+    { name: "sheep", img: require("../assets/img/10.jpg") },
+    { name: "donkey", img: require("../assets/img/11.jpg") },
+    { name: "fox", img: require("../assets/img/12.jpg") },
+    { name: "wolf", img: require("../assets/img/13.jpg") },
+    { name: "bear", img: require("../assets/img/14.jpg") },
+    { name: "hen", img: require("../assets/img/15.jpg") },
+    { name: "deer", img: require("../assets/img/16.jpg") },
+  ];
+  
