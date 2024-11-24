b53af65a14bc4d208a959d315db888fe5dd95a14
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2024-11-24 12:20:47 +0000

    component is called within app.js

--- animal_matching_game/src/App.js
+++ animal_matching_game/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Result from './components/Result';
+
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <h1>ANIMAL MATCHING GAME</h1>
+      <Result />
+
     </div>
   );
 }
